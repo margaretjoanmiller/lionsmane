3f77e76b1044bbf82715ef0891a809d1b06e9e29
Margaret Miller <mmiller@wanderingwires.net>
2025-04-19 08:23:48 -0700

    Add validation dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-hibernate-validator")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-hibernate-orm-panache-kotlin")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
