00c63e312166ff5fe5e9af6cfa8c184a4e5b7988
Margaret Miller <mmiller@wanderingwires.net>
2025-04-19 14:30:26 -0700

    Remove C#

--- LionsManeApi/Tags/Tag.cs
+++
@@ -1,8 +0,0 @@
-namespace LionsManeApi.Tags;
-
-public class Tag
-{
-    public Guid Id { get; set; }
-    public required string Name { get; set; }
-    public required Guid ReaderId { get; set; }
-}
