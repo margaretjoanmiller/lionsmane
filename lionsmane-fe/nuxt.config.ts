7d32f1de9915e57cafd6dbe2299aeda5d6d952f6
Margaret Miller <mmiller@wanderingwires.net>
2025-04-21 12:09:33 -0700

    Setting up for login

--- lionsmane-fe/nuxt.config.ts
+++ lionsmane-fe/nuxt.config.ts
@@ -3,37 +3,48 @@
  */
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-    compatibilityDate: '2024-11-01',
-    devtools: {enabled: true},
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
 
-    modules: [
-        '@nuxt/eslint',
-        '@nuxt/fonts',
-        '@nuxt/icon',
-        '@nuxt/image',
-        '@nuxt/test-utils',
-        'shadcn-nuxt',
-    ],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "shadcn-nuxt",
+    "nuxt-oidc-auth",
+  ],
 
-    css: ['~/assets/css/tailwind.css'],
-    vite: {
-        plugins: [
-            tailwindcss(),
-        ],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  oidc: {
+    defaultProvider: "keycloak",
+    providers: {
+      keycloak: {
+        audience: "account",
+        baseUrl: "",
+        clientId: "",
+        clientSecret: "",
+        redirectUri: "http://localhost:3000/auth/keycloak/callback",
+      },
     },
-
-    shadcn: {
-        /**
-         * Prefix for all the imported component
-         */
-        prefix: '',
-        /**
-         * Directory that the component lives in.
-         * @default "./components/ui"
-         */
-        componentDir: './components/ui'
-    }
-})
+  },
+});
