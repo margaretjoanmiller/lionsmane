0d1a207471bb9a387132dc7299816dac7f34b2a3
Margaret Miller <mmiller@wanderingwires.net>
2025-04-25 02:48:23 -0700

    basic article card, based on the other card format

---
+++ lionsmane-fe/components/ArticleCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+defineProps<{
+  articlePreview: {
+    title: string,
+    preview: string,
+  }
+}>()
+</script>
+
+<template>
+  <Card>
+    <CardTitle>
+      {{articlePreview.title}}
+    </CardTitle>
+    <CardContent>
+      <div class="flex-1">
+        <p class="mt-1">{{articlePreview.preview}}</p>
+      </div>
+    </CardContent>
+  </Card>
+
+</template>
