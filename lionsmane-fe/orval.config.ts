8829f4bb053af10f52892bb496d750d3b3bb3ab2
Margaret Miller <mmiller@wanderingwires.net>
2025-04-26 11:56:17 -0700

    Adding form shadcn and orval generated zod types

---
+++ lionsmane-fe/orval.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'orval';
+
+ 
+
+export default defineConfig({
+
+   lionsmane: {
+
+     output: {
+
+       client: 'zod',
+
+       mode: 'tags',
+
+       target: './utils/gen',
+
+     },
+
+     input: {
+
+       target: './v1.yaml',
+
+     },
+
+   },
+
+ });
