c727ed006092e600d1c973009b11920dce7509ea
Margaret Miller <mmiller@wanderingwires.net>
2025-05-05 00:38:47 -0700

    Tanstack query

--- lionsmane-fe/app.vue
+++ lionsmane-fe/app.vue
@@ -1,8 +1,6 @@
 <!--
   - Copyright (c) 2025 Margaret Miller.  Licensed under the EUPL-1.2-or-later.
   -->
-<script setup lang="ts"></script>
-
 <template>
   <UApp>
     <NuxtLoadingIndicator />
--- lionsmane-fe/nuxt.config.ts
+++ lionsmane-fe/nuxt.config.ts
@@ -3,33 +3,38 @@
  */
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   modules: [
-   "@nuxt/eslint",
-   "@nuxt/fonts",
-   "@nuxt/icon",
-   "@nuxt/image",
-   "@nuxt/test-utils",
-   "shadcn-nuxt",
-   "nuxt-oidc-auth",
-   "nuxt-api-party",
-   "@nuxtjs/color-mode",
-   "@pinia/nuxt",
-   "vue-sonner/nuxt",
-   "@nuxt/ui",
-   "@pinia/colada-nuxt",
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    'shadcn-nuxt',
+    'nuxt-oidc-auth',
+    'nuxt-api-party',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    'vue-sonner/nuxt',
+    '@nuxt/ui',
+    '@peterbud/nuxt-query',
   ],
 
-  css: ["~/assets/css/tailwind.css"],
+  css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
-      include: ["vee-validate", "zod", "@vee-validate/zod"],
+      include: [
+        'vee-validate',
+        'zod',
+        '@vee-validate/zod',
+        '@tanstack/vue-query-devtools',
+      ],
     },
   },
 
@@ -37,39 +42,55 @@ export default defineNuxtConfig({
     /**
      * Prefix for all the imported component
      */
-    prefix: "",
+    prefix: '',
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: "./components/ui",
+    componentDir: './components/ui',
   },
   colorMode: {
-    classSuffix: "",
-    preference: "system",
+    classSuffix: '',
+    preference: 'system',
   },
 
   oidc: {
-    defaultProvider: "keycloak",
+    defaultProvider: 'keycloak',
     middleware: {
       globalMiddlewareEnabled: false,
     },
     providers: {
       keycloak: {
-        baseUrl: "",
-        clientId: "",
-        clientSecret: "",
-        redirectUri: "http://localhost:3000/auth/keycloak/callback",
+        baseUrl: '',
+        clientId: '',
+        clientSecret: '',
+        redirectUri: 'http://localhost:3000/auth/keycloak/callback',
         exposeAccessToken: true,
-        callbackRedirectUrl: "/dashboard",
+        callbackRedirectUrl: '/dashboard',
       },
     },
   },
   apiParty: {
     endpoints: {
       lion: {
-        url: process.env.API_URL || "http://localhost:8080",
-        schema: "./v1.json",
+        url: process.env.API_URL || 'http://localhost:8080',
+        schema: './v1.json',
+      },
+    },
+  },
+
+  nuxtQuery: {
+    autoImports: ['useQuery', 'useMutation'],
+
+    devtools: true,
+
+    // These are the same options as the QueryClient
+    // from @tanstack/vue-query, will be passed
+    // to the QueryClient constructor
+    // More details: https://tanstack.com/query/v5/docs/reference/QueryClient
+    queryClientOptions: {
+      defaultOptions: {
+        queries: {},
       },
     },
   },
