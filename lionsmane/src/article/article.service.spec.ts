f3f74b357c7c021452f835e7e0ba785d16a0b8a3
Margaret Miller <mmiller@wanderingwires.net>
2025-09-02 16:45:43 -0700

    Article service very basic (are we initialized) test

--- lionsmane/src/article/article.service.spec.ts
+++ lionsmane/src/article/article.service.spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticleService } from './article.service';
+import { DrizzlePGModule } from '@knaadh/nestjs-drizzle-pg';
 
 describe('ArticleService', () => {
   let service: ArticleService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        DrizzlePGModule.register({
+          tag: 'DB',
+          pg: {
+            connection: {
+              config: {
+                connectionString:
+                  process.env.DATABASE_URL ||
+                  'postgresql://postgres:postgres@localhost:5432/lionsmane',
+              },
+            },
+          },
+        }),
+      ],
       providers: [ArticleService],
     }).compile();
 
