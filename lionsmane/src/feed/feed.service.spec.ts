0ea4d4f9faa57f544fd3462d68b305ffde0d9c25
Margaret Miller <mmiller@wanderingwires.net>
2025-09-04 18:04:41 -0700

    Improving tests with mocking

--- lionsmane/src/feed/feed.service.spec.ts
+++ lionsmane/src/feed/feed.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FeedService } from './feed.service';
+import { vi } from 'vitest';
 
 describe('FeedService', () => {
   let service: FeedService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FeedService],
+      providers: [
+        FeedService,
+        {
+          provide: 'BullQueue_feed',
+          useValue: {
+            registerQueue: vi.fn().mockReturnValue({ name: 'article' }),
+          },
+        },
+        {
+          provide: 'DB',
+          useValue: {
+            db: vi.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<FeedService>(FeedService);
