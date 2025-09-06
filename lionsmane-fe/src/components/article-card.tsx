8b8621cb0e962b29b65a5e6c5e0aa3952ff3de0c
Margaret Miller <mmiller@wanderingwires.net>
2025-09-05 20:03:27 -0700

    Mark as read

--- lionsmane-fe/src/components/article-card.tsx
+++ lionsmane-fe/src/components/article-card.tsx
@@ -1,13 +1,33 @@
 import { Link } from '@tanstack/react-router';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import type { ArticleDetail } from '@/types/article';
+import { Button } from './ui/button';
+import { $api } from '@/lib/fetch-client';
 
 export function ArticleCard({ article }: { article: ArticleDetail }) {
+  const { mutate, error } = $api.useMutation('patch', '/article/status/{id}');
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>
-          <Link to="/dashboard/$articleId" params={{ articleId: article.id }}>
+          <Link
+            to="/dashboard/$articleId"
+            params={{ articleId: article.id }}
+            onClick={() =>
+              mutate({
+                params: {
+                  path: {
+                    id: article.id,
+                  },
+                  query: {
+                    status: 'read',
+                  },
+                },
+                credentials: 'include',
+              })
+            }
+          >
             {article.title}
           </Link>
         </CardTitle>
