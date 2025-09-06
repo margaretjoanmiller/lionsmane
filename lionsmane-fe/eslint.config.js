1827231e9a695c5fa102ed3c8554800503a6f3fe
Margaret Miller <mmiller@wanderingwires.net>
2025-09-05 22:28:13 -0700

    Eslint plugins

--- lionsmane-fe/eslint.config.js
+++ lionsmane-fe/eslint.config.js
@@ -1,5 +1,11 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
+import { tanstackConfig } from '@tanstack/eslint-config';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  jsxA11y.flatConfigs.recommended,
+  eslintConfigPrettier,
+];
