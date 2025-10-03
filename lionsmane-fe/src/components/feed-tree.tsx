efbc030f707640e0aa8d953882816e9cdc103f0b
Margaret Miller <mmiller@wanderingwires.net>
2025-10-02 20:41:13 -0700

    Fix tree

--- lionsmane-fe/src/components/feed-tree.tsx
+++ lionsmane-fe/src/components/feed-tree.tsx
@@ -1,7 +1,6 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { Link } from '@tanstack/react-router';
 import { Collection, useDragAndDrop } from 'react-aria-components';
-import { useTreeData } from 'react-stately';
 import {
   ContextMenu,
   ContextMenuContent,
@@ -45,71 +44,19 @@ export default function FeedTree({ treeData }: { treeData: FeedTreeData[] }) {
       toast.error('Failed to edit feed', { description: e.message });
     },
   });
-  const tree = useTreeData<FeedTreeData>({
-    initialItems: treeData,
-    getKey: (item) => item.id,
-    getChildren: (item) => item.children || [],
-  });
-
   const { dragAndDropHooks } = useDragAndDrop({
     getItems: (keys) =>
       [...keys].map((key) => {
-        const item = tree.getItem(key)?.value;
+        const item = treeData.find((i) => i.id === key);
         return {
           'text/plain': item?.name || '',
           'application/json': JSON.stringify(item),
         };
       }),
-    onMove(e) {
-      if (e.target.dropPosition === 'before') {
-        tree.moveBefore(e.target.key, e.keys);
-        const key = Array.from(e.keys)[0];
-        const data = tree.getItem(key)?.value;
-        if (data && data.folderId) {
-          editFeed({
-            params: {
-              path: { id: data.id },
-            },
-            credentials: 'include',
-            body: {
-              folderId: null,
-            },
-          });
-        }
-      } else if (e.target.dropPosition === 'after') {
-        tree.moveAfter(e.target.key, e.keys);
-        const key = Array.from(e.keys)[0];
-        const data = tree.getItem(key)?.value;
-        if (data && data.folderId) {
-          editFeed({
-            params: {
-              path: { id: data.id },
-            },
-            credentials: 'include',
-            body: {
-              folderId: null,
-            },
-          });
-        }
-      } else if (e.target.dropPosition === 'on') {
-        // Move items to become children of the target
-        const targetNode = tree.getItem(e.target.key);
-        const targetData = targetNode?.value;
-        if (targetNode && targetData?.type === 'folder') {
-          const targetIndex = targetNode.children
-            ? targetNode.children.length
-            : 0;
-          const keyArray = Array.from(e.keys);
-          for (let i = 0; i < keyArray.length; i++) {
-            tree.move(keyArray[i], e.target.key, targetIndex + i);
-          }
-        }
-      }
-    },
     async onItemDrop(e) {
       const data = e.items.filter((i) => i.kind === 'text')[0];
       const parsed = JSON.parse(await data.getText('application/json'));
-      const target = tree.getItem(e.target.key)?.value;
+      const target = treeData.find((i) => i.id === e.target.key);
       if (e.dropOperation === 'move' && target?.type === 'folder') {
         editFeed({
           params: {
@@ -129,40 +76,39 @@ export default function FeedTree({ treeData }: { treeData: FeedTreeData[] }) {
     <Tree
       aria-label="Feeds and Folders"
       selectionMode="single"
-      items={tree.items}
-      selectedKeys={tree.selectedKeys}
+      items={treeData}
       dragAndDropHooks={dragAndDropHooks}
     >
       {function renderItem(item) {
         return (
-          <TreeItem textValue={item.value.name}>
+          <TreeItem textValue={item.name}>
             <TreeItemContent>
-              {item.value.type === 'feed' ? (
+              {item.type === 'feed' ? (
                 <Link
                   to="/dashboard/feed/$feedId"
                   className="flex flex-row items-center max-w-40 space-x-3"
-                  params={{ feedId: item.value.id }}
+                  params={{ feedId: item.id }}
                 >
-                  {item.value.favicon && (
+                  {item.favicon && (
                     <img
-                      src={item.value.favicon}
-                      alt={`${item.value.name} favicon`}
+                      src={item.favicon}
+                      alt={`${item.name} favicon`}
                       className="max-w-[16px] max-h-[16px]"
                     />
                   )}
-                  <span className="truncate">{item.value.name}</span>
-                  <small>{item.value.unreadCount}</small>
+                  <span className="truncate">{item.name}</span>
+                  <small>{item.unreadCount}</small>
                 </Link>
               ) : (
                 <ContextMenu>
                   <ContextMenuTrigger>
                     <Link
                       to="/dashboard/folder/$folderId"
-                      params={{ folderId: item.value.id }}
+                      params={{ folderId: item.id }}
                       className="flex flex-row items-center max-w-35 space-x-2"
                     >
                       <SolarFolderLineDuotone />
-                      <span className="truncate">{item.value.name}</span>
+                      <span className="truncate">{item.name}</span>
                     </Link>
                   </ContextMenuTrigger>
                   <ContextMenuContent>
@@ -171,7 +117,7 @@ export default function FeedTree({ treeData }: { treeData: FeedTreeData[] }) {
                         deleteFolder({
                           params: {
                             path: {
-                              id: item.value.id,
+                              id: item.id,
                             },
                           },
                           credentials: 'include',
