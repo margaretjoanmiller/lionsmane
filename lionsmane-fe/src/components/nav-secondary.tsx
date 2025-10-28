abc31f256a51b529b680db5b0637464cc0cb571f
Margaret Miller <mmiller@wanderingwires.net>
2025-10-28 16:46:08 -0700

    fix links for sidebar

--- lionsmane-fe/src/components/nav-secondary.tsx
+++ lionsmane-fe/src/components/nav-secondary.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import { Link } from '@tanstack/react-router';
 import { type LucideIcon } from 'lucide-react';
-
+import * as React from 'react';
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -26,10 +26,10 @@ export function NavSecondary({
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild size="sm">
-                <a href={item.url}>
+                <Link to={item.url}>
                   <item.icon />
                   <span>{item.title}</span>
-                </a>
+                </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
