34f7c5d54de982142827d4b4a276f74ff27935a5
Margaret Miller <mmiller@wanderingwires.net>
2025-11-24 20:18:42 -0800

    Fixing deps

--- lionsmane-fe/src/lib/auth-client.ts
+++ lionsmane-fe/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
+import { passkeyClient } from '@better-auth/passkey/client';
 import {
   apiKeyClient,
   inferAdditionalFields,
-  passkeyClient,
   twoFactorClient,
 } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
