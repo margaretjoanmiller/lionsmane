071fcb33f0f0cb25254e57d65e1da215965aca70
Margaret Miller <margaretjoanmiller@pm.me>
2026-03-11 11:02:24 -0700

    Fixed dto

--- lionsmane/src/feed/dto/feed-out.dto.ts
+++ lionsmane/src/feed/dto/feed-out.dto.ts
@@ -2,33 +2,8 @@ import { createSelectSchema } from 'drizzle-zod';
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 import { coreSchema } from '@/db';
-import { FeedMetaData } from '../feed';
 
-export const feedSchema = createSelectSchema(coreSchema.feeds, {
-  metaData: z.custom<FeedMetaData>().nullish(),
-  // lastChecked: z.preprocess((arg: Date | string) => {
-  //   // If the input is a string, try to parse it into a Date object.
-  //   // This handles the '2025-09-01 21:54:33' format.
-  //   if (typeof arg === 'string') {
-  //     return new Date(arg).toISOString();
-  //   } else if (arg instanceof Date) {
-  //     return arg.toISOString();
-  //   }
-  // }, z.iso.datetime()), // Then, validate that the result is a valid ISO datetime string.
-  // updated: z
-  //   .preprocess((arg: Date | string | undefined) => {
-  //     // If the input is a string, try to parse it into a Date object.
-  //     // This handles the '2025-09-01 21:54:33' format.
-  //     if (typeof arg === 'string') {
-  //       return new Date(arg).toISOString();
-  //     } else if (arg instanceof Date) {
-  //       return arg.toISOString();
-  //     } else {
-  //       return null;
-  //     }
-  //   }, z.iso.datetime())
-  //   .nullable(), // Then, validate that the result is a valid ISO datetime string.
-}).extend({
+export const feedSchema = createSelectSchema(coreSchema.feeds).extend({
   lastChecked: z.iso.datetime(),
   updated: z.iso.datetime().nullish(),
 });
