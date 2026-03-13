9a91ceaba8493ac7ccbf108a98c7a9041cfb8fa1
Margaret Miller <margaretjoanmiller@pm.me>
2026-03-13 15:25:01 -0700

    Initial filter mock test

--- lionsmane/src/filter/filter.service.spec.ts
+++ lionsmane/src/filter/filter.service.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { vi } from 'vitest';
+import { coreSchema } from '@/db';
+import { relations } from '@/drizzle/relations';
 import { FilterService } from './filter.service';
 
 describe('FilterService', () => {
@@ -6,7 +10,25 @@ describe('FilterService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilterService],
+      providers: [
+        FilterService,
+        {
+          provide: 'DrizzleAsyncProvider',
+          useValue: drizzle.mock({
+            schema: coreSchema,
+            relations,
+          }),
+        },
+        {
+          provide: 'BullQueue_filter',
+          useClass: vi.fn(
+            class {
+              addBulk = vi.fn();
+              add = vi.fn();
+            },
+          ),
+        },
+      ],
     }).compile();
 
     service = module.get<FilterService>(FilterService);
