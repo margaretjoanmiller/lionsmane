88f491aeeadf86d019e23552497e16a44ca4ce41
Margaret Miller <margaretjoanmiller@pm.me>
2026-03-13 19:21:07 -0700

    Working on tests

--- lionsmane/src/filter/filter.service.spec.ts
+++ lionsmane/src/filter/filter.service.spec.ts
@@ -1,8 +1,10 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { vi } from 'vitest';
 import { coreSchema } from '@/db';
 import { relations } from '@/drizzle/relations';
+import { FilterRule } from './filter';
 import { FilterService } from './filter.service';
 
 describe('FilterService', () => {
@@ -36,5 +38,38 @@ describe('FilterService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should use the correct filter type', () => {
+    const fakeKeywords = faker.helpers.multiple(
+      () => {
+        return faker.word.noun();
+      },
+      {
+        count: 5,
+      },
+    );
+
+    const fakeAuthors = faker.helpers.multiple(
+      () => {
+        return faker.person.fullName();
+      },
+      {
+        count: 5,
+      },
+    );
+    const exampleFilter: FilterRule = {
+      id: 'd09846f4-9103-43ff-b35a-53950958ffcf',
+      userId: 'a0842467-2bf9-431f-b722-3c2fc554245c',
+      conditions: {
+        keywords: fakeKeywords,
+        authors: fakeAuthors,
+      },
+      action: {
+        type: 'blur',
+        contentWarning: faker.word.noun(),
+      },
+      enabled: true,
+    };
   });
 });
