46b937669ad0ec6f70358d1bca08fab1e92d7647
Margaret Miller <margaretjoanmiller@pm.me>
2026-04-03 11:26:18 -0700

    Setting up test data

--- lionsmane/src/parser/parser.service.spec.ts
+++ lionsmane/src/parser/parser.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Rss } from 'feedsmith';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { ParserService } from './parser.service';
 
@@ -17,5 +18,13 @@ describe('ParserService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should normalize an RSS feed item', () => {});
+  it('should normalize an RSS feed item', () => {
+    const item = {
+      title: 'Woah that happened???',
+      link: 'https://example.com/woah-that-happened',
+      pubDate: new Date('2007-12-03T10:15:30Z'),
+      authors: ['Cool Guy', 'Cool Lady'],
+      categories: [{ name: 'TIL', domain: 'https://example.com/til' }],
+    } as Rss.Item<Date>;
+  });
 });
